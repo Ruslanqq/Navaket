a1d1c3dbc2a687e906f868654e1c1bc3f1367eb9
Ruslan <appleruslan7182@gmail.com>
2022-05-19 19:40:33 +0600

    first commit

---
+++ components/AboutUs/aboutus.jsx
@@ -0,0 +1,55 @@
+import Head from "next/head";
+import Image from "next/image";
+import styles from "./aboutus.module.css";
+import React from "react";
+
+const AboutUs = () => {
+  return (
+    <section className={styles.about__us}>
+          <div className={styles.container}>
+            <div className={styles.about__text}>
+              <h3 className={styles.about__h3}>О нас </h3>
+              <div className={styles.info__wrapper}>
+                <p>
+                  Клубный дом находится в южной части города, в самом
+                  экологически чистом его районе, который в то же время оснащен
+                  полноценной инфраструктурой. Жители будут иметь доступ к близ
+                  находящимся развлекательным центрам, супермаркетам, духовным
+                  учреждениям, лучшим школам города, а в радиусе 10 километров
+                  находятся зеленые парки и другие нужные для комфортной жизни
+                  удобства
+                </p>
+                <p>
+                  Уникальная архитектура и дизайн по сей день считаются
+                  характерными чертами клубных домов. Жильцам предоставляются
+                  удобства, доступные только им: парковочные места,
+                  благоустроенные дворы и продуманные коммуникации.
+                </p>
+                <p>
+                  NAVAKET совмещает в себе уют малой резиденции и дух коммуны,
+                  который так необходим для жизни и развития любой ячейки
+                  общества. Будь это молодая пара или супруги с детьми, дом -
+                  одна из основ счастливой семейной жизни.
+                </p>
+              </div>
+            </div>
+            <div className={styles.about__img}>
+              <Image
+                src="/About_us-left.png"
+                alt="hero__img"
+                width={413}
+                height={568}
+              />
+              <Image
+                src="/About_us-right.png"
+                alt="hero__img"
+                width={847}
+                height={468}
+              />
+            </div>
+          </div>
+        </section>
+  );
+};
+
+export default AboutUs;
---
+++ components/Slider/slider.jsx
@@ -0,0 +1,99 @@
+import React, { Component } from "react";
+import Slider from "react-slick";
+import Image from "next/dist/client/image";
+import styles from "./slider.module.css";
+
+
+export default class Responsive extends Component {
+  render() {
+    var settings = {
+      dots: true,
+      infinite: false,
+      speed: 500,
+      slidesToShow: 4,
+      slidesToScroll: 1,
+      initialSlide: 0,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+            initialSlide: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+      ]
+    };
+    return (
+      <div>
+        <div className={styles.container}>
+        <Slider {...settings}>
+          <div>
+            <h4 className={styles.slick__h4}>Изысканный фасад</h4>
+          <Image
+                    src="/slider1.png"
+                    alt="Logo"
+                    width={307}
+                    height={360}
+                  />
+          </div>
+          <div>
+          <h4 className={styles.slick__h4}>Изысканный фасад</h4>
+          <Image
+                    src="/slider2.png"
+                    alt="Logo"
+                    width={307}
+                    height={360}
+                  />
+          </div>
+          <div>
+          <h4 className={styles.slick__h4}>Изысканный фасад</h4>
+          <Image
+                    src="/slider3.png"
+                    alt="Logo"
+                    width={307}
+                    height={360}
+                  />
+          </div>
+          <div>
+          <h4 className={styles.slick__h4}>Изысканный фасад</h4>
+          <Image
+                    src="/slider4.png"
+                    alt="Logo"
+                    width={307}
+                    height={360}
+                  />
+          </div>
+          <div>
+          <h4 className={styles.slick__h4}>Изысканный фасад</h4>
+          <Image
+                    src="/slider5.png"
+                    alt="Logo"
+                    width={307}
+                    height={360}
+                  />
+          </div>
+          <div>
+          </div>
+        </Slider>
+        </div>
+      </div>
+    );
+  }
+}
